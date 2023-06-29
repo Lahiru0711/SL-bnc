4a553e4fefbbe106f0ebeb8e1310c429a8dd286d
lahiru indusara <105131558+Lahiru0711@users.noreply.github.com>
2023-06-29 19:00:20 +0530

    Create ContactUs.php

---
+++ Project/ContactUs.php
@@ -0,0 +1,182 @@
+<!DOCTYPE HTML>
+<HTML lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Contact Us">
+    <meta name="keywords"
+        content="SL_bnc Contact,SL_bnc,Vehicle Reservation,Vehicle Reservation srilanka,Srilankan Vehicle">
+    <meta http-equiv="refresh" content="1200">
+
+
+    <title>Contact Us</title>
+
+    <!-- link css,js files -->
+
+    <link rel="stylesheet" href="css/ContactUs.css">
+    <script src=""></script>
+
+</head>
+
+<body>
+
+    <div class="wrapper">
+        <div class="logo">
+            <img src="img/45.png">
+        </div>
+        <!--logo-->
+
+
+        <div class="home">
+            <nav class="clearfix">
+                <ul>
+                    <li><a href="index.php"> home </a></li>
+                    <li><a href="maincat.php"> vehicles</a></li>
+                    <li><a href="AboutUs.php"> about us</a></li>
+                    <li><a href="ContactUs.php"> customer care</a></li>
+                    <li><a href="register_1.php"> register now</a></li>
+                    <li><a href="select1.php"> login</a></li>
+                    <li><a href="select.php"> my account</a></li>
+                </ul>
+            </nav>
+
+        </div>
+
+    </div>
+
+
+
+    <div class="Headline">
+        <h1>How Can We Help?</h1>
+
+        <br><br>
+
+        <p>
+
+            If you have any inquiry, Please fill out our contact form.
+
+        </p>
+
+    </div>
+
+
+
+
+
+
+
+
+    <div class="form">
+        <form action="" method="POST">
+
+            <H3>Contact form</H3>
+            <br><br>
+
+            <input style="width: inherit;" type="radio" id="Cus" value="Customer"><label>Customer</label>
+            <input style="width: inherit;" type="radio" id="Dri" value="Driver"><label>Driver</label>
+
+            <br><br><br>
+
+            <input type="text" id="C_1" placeholder="Enter Your Name" required size="50">
+
+            <br><br><br>
+
+            <input type="email" id="C_2" placeholder="Enter Your Email" required>
+
+            <br><br><br>
+
+            <textarea id="C_3" placeholder="Enter Your Message" required cols="75" rows="10"></textarea>
+            <br>
+
+
+            <button type="submit">Submit</button>
+
+        </form>
+
+    </div>
+
+
+    <div class="contact">
+
+        <div class="address">
+
+            <h2>How to find us</h2>
+
+
+            <img src="img/contactus/location.png">
+
+            <p>
+                NO:20/5,<br>
+                Sl bnc LTD,<br>
+                Galle Rd,Colombo 10
+            </p>
+
+        </div>
+
+        <div class="con">
+
+            <h2>Get in touch</h2>
+
+            <div class="tele">
+                <h3>Telephone</h3>
+
+                <img src="img/contactus/phone.png">
+
+                <p>(+94)112345678</p>
+
+            </div>
+
+
+            <div class="email">
+
+                <h3>Email</h3>
+
+                <img src="img/contactus/email.png">
+
+                <p>Info@Sl_bnc.com</p>
+
+            </div>
+
+
+
+        </div>
+
+
+
+        <div class="social">
+
+            <h2>Stay conected</h2>
+
+
+
+            <a href="" id="s1"><img src="img/contactus/s1.png"></a>
+            <a href="" id="s2"><img src="img/contactus/s2.png"></a>
+            <a href="" id="s3"><img src="img/contactus/s3.png"></a>
+            <a href="" id="s4"><img src="img/contactus/s4.png"></a>
+            <a href="" id="s5"><img src="img/contactus/s5.png"></a>
+
+
+
+
+
+        </div>
+
+
+    </div>
+
+
+
+    <footer class="footer">
+        <marquee behavior="" direction="right">
+            <p> &copy;<cite>2021 Sl_bnc Ltd. All rights reserved</cite></p>
+        </marquee>
+
+    </footer>
+
+    </div>
+
+</body>
+
+</HTML>
