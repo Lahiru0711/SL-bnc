a80fb14ce5e483dcbb386bbe99c0973e4082c59f
lahiru indusara <105131558+Lahiru0711@users.noreply.github.com>
2023-06-29 19:00:42 +0530

    Create AboutUs.php

---
+++ Project/AboutUs.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="css/AboutUs.css">
+	
+</head>
+
+<body>
+   <div class="wrapper">
+    <div class="logo">
+             <img src="img/45.png">
+	</div> <!--logo-->
+	
+	
+	 <div class="home">
+	     <nav class="clearfix">
+		     <ul>
+			     <li><a href="index.php"> home </a></li>
+				 <li><a href="maincat.php"> vehicles</a></li>
+				 <li><a href="AboutUs.php"> about us</a></li>
+				 <li><a href="ContactUs.php"> customer care</a></li>
+				 <li><a href="register_1.php">  register now</a></li>
+				 <li><a href="select1.php"> login</a></li>
+				 <li><a href="select.php"> my account</a></li>
+			 </ul>
+		 </nav>
+	   
+	 </div> <!--home-->
+	 </div> <!--wrapper-->
+	 <div class = "wrapper content">
+	 <p style = "font-size:60px; margin:10px auto; color:rgba(252,183,80);">About Us</p>
+	 <br>
+	 
+	 <p style = "font-size:25px; margin:10px auto; color:rgba(252,183,80);text-align:left !important;">Our Vision</p>
+	 <br>
+	 
+	     <img src="img/21.png" style = "width:800px;">
+			 
+	 <p style = "overflow:auto; max-height:200px !important"><br>
+	             As the market leader and Sri Lanka's most successful start-up business in recent times, we envision the intelligent upgrade of the local transportation industry; consequently inspiring Sri Lanka towards matching global standards in effective problem solving via technology.
+				 <br>
+				 <br>
+				 <br>
+	    		"SL bnc" envisions the intelligent upgrade of mobility, consequently inspiring Sri Lanka towards matching global standards in effective problem solving via technology. Our vehicle reservation system is committed to innovate and build solutions that push boundaries in the mobility space using advanced techno user is committing to becoming a fully electric, zero-emission platform by 2025, with 100% of rides taking place in zero-emission vehicles, on public transit. It is our responsibility as the largest mobility platform in the sri lanka, to more aggressively tackle the challenge of climate change. We will do this by offering riders more ways to ride green, helping drivers go electric, making transparency a priority and partnering with NGOs and the private sector to help expedite a clean and just energy transition. logy and Big Data. Our mission is to upgrade the efficiency, reliability, and safety of the country's mobility services by introducing technology, standards, order, and convenience into the system. 
+				 <br>
+				 <br>
+				 <br>
+				"SL bnc" is the proud product of this vision, instilled by our leadership that comprises the enterprisingly youthful and highly accomplished, and realized by talent warehoused at our headquarters in Colombo, Sri Lanka.
+				 		 
+	</p>
+	<br>
+	
+	<p style = "font-size:25px; margin:10px auto; color:rgba(252,183,80);text-align:left !important;">Our Operation</p>
+	<br>
+	
+	     <img src="img/22.png" style = "width:300px;">
+	
+			    <p style = "overflow:auto; max-height:300px !important"><br>
+	            We are a proud homegrown technology solutions provider specializing in mobility solutions, striving towards alleviating the mobility struggles we all face in our day-to-day lives. As a company, it is a privilege to be a forerunner in this domain, as we understand and can relate to the needs of our people intimately, and thereby have the unique advantage of developing tailor-made solutions that are safe, sustainable and swift to the public.
+				<br>
+				<br>
+				<br>
+				Whether you’re in the back seat or behind the wheel, your safety is essential. We are committed to doing our part, and technology is at the heart of our approach. We partner with safety advocates and develop new technologies and systems to help improve safety and help make it easier for everyone to get around.
+				<br>
+				<br>
+				<br>
+				The vehicle reservation software is a platform that facilitates a real-time connection between customer and the  driver, enabling mutual engagement for the receipt and reservation for a seamless service.
+				<br>
+				<br>
+				We continue providing the best of the tech, evaluating ourselves each step of the way.
+		 
+	</p>
+	
+	 
